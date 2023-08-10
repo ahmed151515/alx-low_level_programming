9beaeacca8b04ffff54d0ffec412b22e676ff805
ahmed <ahmedarafa.200444@gmail.com>
2023-08-10 03:03:22 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * main - fv,fmvgvb
+ * Return: 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	for (int i = 0; i < 59; i++)
+
+	putchar(quote[i]);
+	return (1);
+}
