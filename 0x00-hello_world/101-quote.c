6b05b7a60458e9d3f1792f739ef908b0033dedaa
ahmed <ahmedarafa.200444@gmail.com>
2023-08-10 03:49:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,10 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[] = "and that piece of art is useful\"\
+- Dora Korpar, 2015-10-19\n";
 	int i;
+
 	for (i = 0; i < 59; i++)
 
 	putchar(quote[i]);
