86033755a672063daa31acbc24e88542286370bb
ahmed <ahmedarafa.200444@gmail.com>
2023-08-10 01:26:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
  * main funcsion - print "\Programming is like building a multilingual puzzle"
  * Description: This is a longer description.
  * this code to solve proplom done
+ * i use puts functios
  * Return: 0
  */
 int main(void)
