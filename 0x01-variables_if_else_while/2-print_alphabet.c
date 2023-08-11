632ebf65d023241eaeaf0c6a2ea63baa73b5c7b9
ahmed <ahmedarafa.200444@gmail.com>
2023-08-11 00:17:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry funtion
+ * Description: main have all code
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char tmp = 'a';
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(tmp);
+		tmp++;
+	}
+	putchar('\n');
+	return (0);
+}
