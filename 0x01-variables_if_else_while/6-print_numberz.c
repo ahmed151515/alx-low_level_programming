5101148cdce270a3a2fbf4be9b18d64864b8ef8a
ahmed <ahmedarafa.200444@gmail.com>
2023-08-11 01:26:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry funtion
+ * Description: main have all code
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar((char) i);
+	}
+	putchar('\n');
+	return (0);
+}
