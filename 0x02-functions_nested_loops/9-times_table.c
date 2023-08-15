ecb4c716e7a0aead9ee1689d23158262b0cb6dda
ahmed <ahmedarafa.200444@gmail.com>
2023-08-15 09:28:06 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 			if (tmp < 10)
 			{
 				_putchar(tmp + '0');
-				if (j < 8)
+				if (j < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -25,7 +25,7 @@ void times_table(void)
 
 				_putchar((tmp / 10) + '0');
 				_putchar((tmp % 10) + '0');
-				if (j < 8)
+				if (j < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
