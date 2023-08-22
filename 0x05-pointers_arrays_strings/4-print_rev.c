654d61dfc4065f8de716bae153a73dd6491e264c
ahmed <ahmedarafa.200444@gmail.com>
2023-08-22 06:12:48 -0700

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - print_rev
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+}
