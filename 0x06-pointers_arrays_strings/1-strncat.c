4a9690ae570d171e1802858dd7f2c3514d0a2cb8
ahmed <ahmedarafa.200444@gmail.com>
2023-08-24 01:47:44 -0700

    .

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,9 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 	int len_dest, i;
 	for (len_dest = 0; dest[len_dest] != '\0'; len_dest++)
 		;
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[len_dest + i] = src[i];
 	}
 	dest[len_dest + i] = '\0';
+	return (dest);
 }
