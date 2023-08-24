2f833d15f1e4446d720754c6961c782fe1ee8095
ahmed <ahmedarafa.200444@gmail.com>
2023-08-24 01:13:35 -0700

    .

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - _strncpy
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	char *res = src;
+
+	while (n > 0)
+	{
+		src++;
+		n--;
+	}
+	while (*dest != '\0')
+	{
+		*src = *dest;
+		dest++;
+		src++;
+	}
+	*src = '\0';
+	dest = res;
+	return (res);
+}
