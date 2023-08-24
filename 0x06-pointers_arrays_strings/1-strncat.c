23e11c6a76775eb14c26fdf0926b5ec2c5061db0
ahmed <ahmedarafa.200444@gmail.com>
2023-08-24 00:43:52 -0700

    .

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - _strncat
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *res = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	/**dest = '\0';*/
+	return (res);
+}
