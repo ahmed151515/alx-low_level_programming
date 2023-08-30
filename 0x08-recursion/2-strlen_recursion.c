0abdb5d8cb426d864e068de64b158f566e2c324d
ahmed <ahmedarafa.200444@gmail.com>
2023-08-30 05:48:00 -0700

    .

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,24 @@
 #include "main.h"
 /**
+ * tmp - tmp
+ * @s: string
+ * @ count: int
+ * Retunr: int
+ */
+int tmp(char *s, int count = 0)
+{
+	if (*s == '\0')
+	{
+		return (count);
+	}
+	return (tmp(s + 1, count + 1));
+}
+/**
+ * _strlen_recursion - _strlen_recursion
+ * @s: string
+ * Return: int
  */
 int _strlen_recursion(char *s)
 {
-
+	return (tmp(s));
 }
