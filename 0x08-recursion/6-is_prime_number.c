88f3e32936e14438394127c6d71f868dfdb27f19
ahmed <ahmedarafa.200444@gmail.com>
2023-08-30 05:03:24 -0700

    .

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,14 +1,32 @@
 #include "main.h"
 /**
+ * tmp -tmp
+ * @n: int
+ * @d: int
+ * Return: int
+ */
+int tmp(int n, int d)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (d * d > n)
+	{
+		return (1);
+	}
+	if (n % d == 0)
+	{
+		return (0);
+	}
+	return (tmp(n, d + 1));
+}
+/**
  * is_prime_number - is_prime_number
  * @n: number
  * Return: int
  */
 int is_prime_number(int n)
 {
-	if (n % 2 != 0)
-	{
-		return (0);
-	}
-	return (n % 2 && is_prime_number(n - 1));
+	return (tmp(n, 2));
 }
