302ce1422c936319b703aa9e15c558fd81d63db1
ahmed <ahmedarafa.200444@gmail.com>
2023-09-01 12:04:38 -0700

    .

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * main - main
+ * @argv: araay
+ * @argc: int
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int num[] = {25, 10, 5, 2, 1};
+	int res = 0, i, n;
+
+	n = atoi(argv[1]);
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	if (argc < 0)
+	{
+		printf("oo\n");
+		return (0);
+	}
+	for (i = 0; i < 5; i++)
+	{
+		if (n >= num[i])
+		{
+			res += n / num[i];
+			n -= res * num[i];
+			if (n == 0)
+			{
+				break;
+			}
+		}
+	}
+	printf("%d\n", res);
+	return (0);
+}
