a37256dbd207c26654e864a4d52e61b98335f683
ahmed <ahmedarafa.200444@gmail.com>
2023-09-01 10:16:25 -0700

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - main
+ * @argv: araay
+ * Retrun: int
+ */
+int main(char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+int _putchar(char c);
+#endif
