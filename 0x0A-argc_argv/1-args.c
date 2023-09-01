c1083e050325e5a3f6f27bd5a3fa00f4b9a9d3c5
ahmed <ahmedarafa.200444@gmail.com>
2023-09-01 10:30:49 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - main
+ * @argv: araay
+ * @argc: int
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
