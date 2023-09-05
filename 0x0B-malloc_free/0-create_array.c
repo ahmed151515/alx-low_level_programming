7a55b8ba7adfac04304a44a4cee5f049475bd881
ahmed <ahmedarafa.200444@gmail.com>
2023-09-05 02:33:28 -0700

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,12 +3,21 @@
  * create_array - create array
  * @size: len of array
  * @c: valu to initializes
- * Return: pointer the array
+ * Return: pointer the array or NULL if size = 0 or array faul
  */
 char *create_array(unsigned int size, char c)
 {
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	char *res = (char *)malloc(sizeof(c) * size);
 	unsigned int i;
+
+	if (res == NULL)
+	{
+		return (res);
+	}
 
 	for (i = 0; i < size; i++)
 	{
