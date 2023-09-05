b4423f9f294f1e919412d8ecdc1959d174d46834
ahmed <ahmedarafa.200444@gmail.com>
2023-09-05 03:49:18 -0700

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,10 +10,10 @@ char *str_concat(char *s1, char *s2)
 	char *res;
 	int len1, len2, i, j;
 
-	if (s1[0] == '\0' || s2[0] == '\0')
+/*	if (s1[0] == '\0' || s2[0] == '\0')
 	{
 		return (NULL);
-	}
+	}*/
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
