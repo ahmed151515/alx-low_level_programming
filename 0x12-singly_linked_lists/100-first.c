98ce638726d3500792ea167f5d5762326d1fabb2
ahmed <ahmedarafa.200444@gmail.com>
2023-09-21 04:46:24 -0700

    .

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void print(void) __attribute__((constructor));
+/**
+ * print - print
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
