5e2c74d7414cc63a3ffea834ba63c3eccf6ef241
ahmed <ahmedarafa.200444@gmail.com>
2023-09-21 04:28:35 -0700

    .

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,6 +7,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+	int len = 0;
 
 	for (; h != NULL; h = h->next)
 	{
@@ -16,7 +17,9 @@ size_t print_list(const list_t *h)
 			printf("[0] (nill)\n");
 			continue;
 		}
-		printf("[%d] %s\n", h->len, h->str);
+		for (; str[len] != '\0'; len++)
+			;
+		printf("[%d] %s\n", len, h->str);
 	}
 	return (count);
 }
