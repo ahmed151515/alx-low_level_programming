5eb24f4a2b1b5d3f9ffd668da5e0f36c36605bcc
ahmed <ahmedarafa.200444@gmail.com>
2023-09-28 06:21:11 -0700

    .

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_bit - get_bit
+ * @n: n
+ * @index: idex
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n & (1 << index))
+	{
+		return (1);
+	}
+	return (0);
+}
