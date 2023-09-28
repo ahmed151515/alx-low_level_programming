910209f0de389dfce0469e857d4ea665275cb466
ahmed <ahmedarafa.200444@gmail.com>
2023-09-28 05:34:02 -0700

    .

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint - binary_to_uint
+ * @b: 0 1
+ * Return: int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int len = sizeof(b) - 1;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+
+		num += (*b - '0') * num;
+		b++;
+	}
+	return (num);
+}
