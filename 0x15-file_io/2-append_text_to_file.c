000c0e0e027bbee5c4cd9d8c97f344b970c07449
ahmed <ahmedarafa.200444@gmail.com>
2023-10-02 11:07:07 -0700

    .

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,12 +16,12 @@ int size_stirng(char *s)
 	return (size);
 }
 /**
- * create_file - create_file
+ * append_text_to_file - append_text_to_file
  * @filename: same
  * @text_content: same
  * Return:int
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int f;
 	int test = 0, len = size_stirng(text_content);
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	f = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+	f = open(filename, O_ APPEND | O_WRONLY);
 	if (f == -1)
 	{
 		return (-1);
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 		close(f);
 		return (1);
 	}
-	test = write(f, text_content, size_stirng(text_content));
+	test = write(f, text_content, len);
 	if (test == -1)
 	{
 		return (-1);
