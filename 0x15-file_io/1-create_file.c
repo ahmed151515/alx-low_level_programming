35cbc00ce80a3da712581cacdc3dd62bdc29f97d
ahmed <ahmedarafa.200444@gmail.com>
2023-10-02 09:56:25 -0700

    .

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - create_file
+ * @filename: same
+ * @text_content: same
+ * Return:int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	int test;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	f = open(filename, O_WRONLY, 0600);
+	if (f == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	test = write(f, text_content, sizeof(text_content) - 1);
+	if (test == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
