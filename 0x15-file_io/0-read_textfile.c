64c87e69fc359a1647b64881c4b94dc0afd3e29e
ahmed <ahmedarafa.200444@gmail.com>
2023-10-02 08:45:51 -0700

    .

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,24 +8,26 @@
 size_t read_textfile(const char *filename, size_t letters)
 {
 	size_t count = 0;
-	FILE *f;
-	char c = '1';
+	int f;
+	char *c = malloc(letters + 1);
 
-	if (filename == NULL)
+	c[letters + 1] = '\0';
+
+	if (filename == NULL || c == NULL)
 	{
 		return (0);
 	}
-	f = fopen(filename, "r");
-	if (f == NULL)
+	f = open(filename, O_RDONLY);
+	if (f == -1)
 	{
 		return (0);
 	}
 
-	while (letters-- && c)
+	read(f, c, letters);
+	while (*c != '\0')
 	{
-		c = getc(f);
-
-		_putchar(c);
+		_putchar(*c);
+		c++;
 		count++;
 	}
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
