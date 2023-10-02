c9b50c062bea515151235436fb3623714ca464f0
ahmed <ahmedarafa.200444@gmail.com>
2023-10-02 10:07:43 -0700

    .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	f = open(filename, O_WRONLY | O_TRUNC, O_CREAT, S_IWUSR | S_IRUSR);
+	f = open(filename, O_WRONLY | O_TRUNC, O_CREAT, 0600);
 	if (f == -1)
 	{
 		return (-1);
@@ -26,5 +26,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	close(f);
 	return (1);
 }
