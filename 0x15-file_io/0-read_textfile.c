504f3ff353af4784315cb6b5e12f17b519841537
ahmed <ahmedarafa.200444@gmail.com>
2023-10-02 09:29:27 -0700

    .

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ size_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	f = open(filename, O_RDONLY);
+	f = open(filename, O_RDONLY, 0600);
 	if (f == -1)
 	{
 		return (0);
