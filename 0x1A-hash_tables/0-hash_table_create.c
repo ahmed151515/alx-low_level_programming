ee845a3712395a55ded65926937b24aeecbb0054
ahmed <ahmedarafa.200444@gmail.com>
2023-12-01 06:27:50 -0800

    .

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new = malloc(siezof(hash_table_t));
+	hash_table_t *new = malloc(sizeof(hash_table_t));
 
 	if (new == NULL)
 	{
