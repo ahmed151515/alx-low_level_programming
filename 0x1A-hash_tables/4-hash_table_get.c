0f972212407eae9649a7b96380ab6d0c099fdf09
ahmed <ahmedarafa.200444@gmail.com>
2023-12-01 09:11:50 -0800

    .

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,18 +7,17 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	char *value;
 	hash_node_t *cur;
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
-	if (strcmo(ht->array[index]->key, key) == 0)
+	if (strcmp(ht->array[index]->key, key) == 0)
 	{
 		return (ht->array[index]->value);
 	}
 	else
 	{
 		cur = ht->array[index];
-		while (strcmo(cur->key, key) != 0)
+		while (strcmp(cur->key, key) != 0)
 		{
 			cur = cur->next;
 		}
