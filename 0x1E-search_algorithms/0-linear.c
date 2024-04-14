cd0ba25525baac6817a1b6c13cc5504ca466c86c
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-04-14 13:55:34 +0200

    .

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear_search
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: positon
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%d] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+        {
+            return (i);
+        }
+    }
+    return (-1);
+}
