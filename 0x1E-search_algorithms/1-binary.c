d9f64b518cfbdfbc2d2b643feda849f2a4e2b1a5
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-04-14 14:43:13 +0200

    .

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - binary_search
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: positon
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t high = size - 1, low = 0, mid, i;
+
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+
+		mid = (low + (high - low) / 2);
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] > value)
+		{
+			high = mid - 1;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+	}
+	return (-1);
+}
